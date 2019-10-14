42cee36a384dc1ead590f16102fe096be73d62fd
Slava Kirshman <slava.kirshman@icloud.com>
2019-10-14 11:57:55 +0300

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./reset.css"
+import classes from "./App.module.css"
+
+function App() {
+  return (
+    <div>
+      <header>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
