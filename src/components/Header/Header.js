6512b4963da6cc3c33c2b88b736bf05afe130ad9
Slava Kirshman <slava.kirshman@icloud.com>
2019-11-11 19:52:11 +0200

    Header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import * as PropTypes from "prop-types";
+// import classes from "./Header.module.css";
+
+function Header() {
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+Header.propTypes = {
+
+};
+
+// export default Header;
